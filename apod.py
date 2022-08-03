e025dcc6ffedac70800c2692008b5a13c0fa7fe1
Matt Witt <wittsend86@gmail.com>
2022-08-03 07:39:51 +0100

    Attempting to fix problems with bundling

--- apod.py
+++ apod.py
@@ -3,20 +3,38 @@
 # 2022-08-02
 
 import requests
-import sys, os
+import sys
+import os
 from datetime import date
 from mimetypes import guess_extension
+
+
+
+def get_app_path():
+    if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the PyInstaller bootloader
+        # extends the sys module by a flag frozen=True and sets the app 
+        # path into variable _MEIPASS'.
+        application_path = sys._MEIPASS
+    else:
+        application_path = os.path.dirname(os.path.abspath(__file__))
+    return application_path
+
+
+
+
+app_path = get_app_path()
 
 # Get API key from apikey file in script path
 # (See apikey.example)
 try:
-    print(f'Fetching API key from "{sys.path[0]}{os.sep}apikey.txt"')
-    with open(f'{sys.path[0]}{os.sep}apikey.txt', 'r') as apikey_file:
+    print(f'Fetching API key from "{app_path}{os.sep}apikey.txt"')
+    with open(f'{app_path}{os.sep}apikey.txt', 'r') as apikey_file:
         api_key=apikey_file.read().strip()
     print(f'API key is "{api_key}"')
 except:
     print("No apikey.txt file found.")
-    quit()
+    sys.exit()
 
 api_parms = {
     'api_key':      api_key,
@@ -24,7 +42,7 @@ api_parms = {
 }
 api_url = f'https://api.nasa.gov/planetary/apod'
 
-save_path = f'{sys.path[0]}{os.sep}received{os.sep}' 
+save_path = f'{app_path}{os.sep}received{os.sep}' 
 save_file = f'{save_path}apod-{api_parms["date"]}'
 
 print(f'Requesting data from {api_url}...')
@@ -34,7 +52,7 @@ resp = requests.api.get(url=api_url, params=api_parms)
 # If there was an error
 if(resp.status_code != 200):
     print(resp.reason)
-    quit()
+    sys.exit()
 
 # Convert received JSON string to dictionary
 data = resp.json()
@@ -44,14 +62,14 @@ im = requests.get(data["hdurl"])
 #print(im.headers)
 if im.status_code != 200:
     print(im.reason)
-    quit()
+    sys.exit()
 
 f_ext = guess_extension(im.headers["content-type"])
 
 if not os.path.exists(save_path):
     os.mkdir(save_path)
 
-print(f'Saving image to "{save_file}{f_ext}...\n"')
+print(f'Saving image to "{save_file}{f_ext}..."\n')
 with open(rf'{save_file}{f_ext}', 'wb') as f:
     f.write(im.content)
 
